426a79ea8b3f75b7840065587c7f745c1f33fe6a
Ruhi Singh <ruhis@adobe.com>
2023-10-04 18:35:06 +0530

    EXLM-55 MD converter for video

--- src/modules/blocks/create-video.js
+++ src/modules/blocks/create-video.js
@@ -4,19 +4,19 @@ export default function createVideo(document) {
     const videoElements = Array.from(document.getElementsByClassName("extension video"));
     videoElements.forEach(function (element) {
 		
-		const div = document.createElement('div');
+        const div = document.createElement('div');
         const img = document.createElement('img');
         const href = element.querySelector('iframe').src;
-		
-		const p1 = document.createElement('p');
-		p1.append(img);
-		
-		const p2 = document.createElement('p');
-		p2.innerHTML = '<a href="'+href+'">'+href+'</a>';
-		
-		div.append(p1); 
-		div.append(p2);
-		
+
+        const p1 = document.createElement('p');
+        p1.append(img);
+
+        const p2 = document.createElement('p');
+        p2.innerHTML = '<a href="' + href + '">' + href + '</a>';
+
+        div.append(p1);
+        div.append(p2);
+
         const cells = [
             ['embed'],
             [div],
