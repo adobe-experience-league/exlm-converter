a64cdbd76c2f2ff65727a47f119a50fdcd26972c
Ruhi Singh <ruhis@adobe.com>
2023-10-05 21:56:13 +0530

    EXLM 89 MD converter for Note component

--- src/modules/blocks/create-note.js
+++ src/modules/blocks/create-note.js
@@ -1,17 +1,20 @@
 import * as WebImporter from '@adobe/helix-importer'
 
-export default function createNote(document) {
-  const noteElements = Array.from(
-    document.getElementsByClassName('extension note')
-  )
+export default function createNote (document) {
+  // Notes have class extension along with the variation name.
+  const noteElements = Array.from(document.querySelectorAll('.extension:not(.relatedarticles):not(.video)'))
+
   noteElements.forEach((el) => {
-    const cells = [['note']]
+    el.classList.remove('extension')
+    const variation = el.classList[0]
+    const cells = [[`note (${variation})`]]
+
+    // Row for each divs inside a note
     Array.from(el.children).forEach((innerDiv) => {
       const div = document.createElement('div')
       div.innerHTML = `<p>${innerDiv.textContent}</p>`
       cells.push([div])
     })
-
     const block = WebImporter.DOMUtils.createTable(cells, document)
     el.parentNode.replaceChild(block, el)
   })
