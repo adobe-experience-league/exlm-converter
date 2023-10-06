71352d0887930baa46700eccf48aef1d116156a9
Ruhi Singh <ruhis@adobe.com>
2023-10-06 14:45:46 +0530

    EXLM 55 Code refactoring for video component

--- src/modules/blocks/create-video.js
+++ src/modules/blocks/create-video.js
@@ -1,30 +1,16 @@
-import * as WebImporter from '@adobe/helix-importer';
+import * as WebImporter from '@adobe/helix-importer'
 
-export default function createVideo(document) {
-    const videoElements = Array.from(document.getElementsByClassName("extension video"));
-    videoElements.forEach(function (element) {
-		
-        const div = document.createElement('div');
-        const img = document.createElement('img');
-        const href = element.querySelector('iframe').src;
-
-        const p1 = document.createElement('p');
-        p1.append(img);
-
-        const p2 = document.createElement('p');
-        p2.innerHTML = `<a href="${href}">${href}</a>`;
-
-        div.append(p1);
-        div.append(p2);
-
-        const cells = [
-            ['embed'],
-            [div],
-        ]; 
-		
-        const block = WebImporter.DOMUtils.createTable(cells, document);
-        element.parentNode.replaceChild(block, element);
-    });
+export default function createVideo (document) {
+  const videoElements = Array.from(
+    document.getElementsByClassName('extension video')
+  )
+  videoElements.forEach(element => {
+    const iframe = element.querySelector('iframe')
+    const href = iframe ? iframe.src : ''
+    const div = document.createElement('div')
+    div.innerHTML = ` <p><img></p><p><a href="${href}">${href}</a></p> `
+    const cells = [['embed'], [div]]
+    const block = WebImporter.DOMUtils.createTable(cells, document)
+    element.parentNode.replaceChild(block, element)
+  })
 }
-
-
