56d899bec3c74e8a9e584fc50d56621f47b24917
Ruhi Singh <ruhis@adobe.com>
2023-10-06 16:28:24 +0530

    EXLM 55 Code refactoring for video component

--- src/modules/blocks/create-video.js
+++ src/modules/blocks/create-video.js
@@ -4,13 +4,17 @@ export default function createVideo (document) {
   const videoElements = Array.from(
     document.getElementsByClassName('extension video')
   )
-  videoElements.forEach(element => {
-    const iframe = element.querySelector('iframe')
-    const href = iframe ? iframe.src : ''
-    const div = document.createElement('div')
-    div.innerHTML = ` <p><img></p><p><a href="${href}">${href}</a></p> `
-    const cells = [['embed'], [div]]
-    const block = WebImporter.DOMUtils.createTable(cells, document)
-    element.parentNode.replaceChild(block, element)
-  })
+  if (videoElements.length) {
+    videoElements.forEach(element => {
+      const iframe = element.querySelector('iframe')
+      // If iframe exists, get the src attribute else iframe source will be passed as empty
+      const href = iframe ? iframe.src : ''
+      const div = document.createElement('div')
+      div.innerHTML = ` <p><img></p><p><a href="${href}">${href}</a></p> `
+      // create the embed block and append it to the main element
+      const cells = [['embed'], [div]]
+      const block = WebImporter.DOMUtils.createTable(cells, document)
+      element.parentNode.replaceChild(block, element)
+    })
+  }
 }
