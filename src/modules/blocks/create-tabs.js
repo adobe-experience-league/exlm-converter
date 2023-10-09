5ea6cd9b9fb914fbdba65e797f9af0ca8a2733f9
Ruhi Singh <ruhis@adobe.com>
2023-10-09 11:03:13 +0530

    Tabs

--- src/modules/blocks/create-tabs.js
+++ src/modules/blocks/create-tabs.js
@@ -6,19 +6,17 @@ export default function createTabs(document) {
     tabElements.forEach((element) => {
       // Check if the element is tab
       if (element.firstElementChild.nodeName === 'SP-TABS') {
-        const divTabName = document.createElement('div');
-        const divTabValue = document.createElement('div');
         const tabs = element.querySelectorAll('sp-tabs > sp-tab');
         const tabsPanels = element.querySelectorAll('sp-tabs > sp-tab-panel');
-        tabs.forEach((tab) => {
-          divTabName.innerHTML += `<div><p>${tab.getAttribute(
-            'label',
-          )}</p></div>`;
+        const cells = [['tabs']];
+        tabs.forEach((tab, i) => {
+          const tabName = document.createElement('div');
+          const tabValue = document.createElement('div');
+          tabName.innerHTML = `<p>${tab.getAttribute('label')}</p>`;
+          tabValue.innerHTML = `<p>${tabsPanels[i].textContent}</p>`;
+          cells.push([[tabName], [tabValue]]);
         });
-        tabsPanels.forEach((tabsPanel) => {
-          divTabValue.innerHTML += `<div><p>${tabsPanel.textContent}</p></div>`;
-        });
-        const cells = [['tabs'], [[divTabName], [divTabValue]]];
+
         const block = WebImporter.DOMUtils.createTable(cells, document);
         element.parentNode.replaceChild(block, element);
       }
