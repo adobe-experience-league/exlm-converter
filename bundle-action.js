2a47aa242008ac2a7cbe1a90ddef2a8c9b9615a1
Ahmed <musallam@adobe.com>
2023-10-11 02:17:59 -0500

    remove webpack, use esbuild - FAST and no frills

--- bundle-action.js
+++ bundle-action.js
@@ -1,51 +1,40 @@
-#!/usr/bin/env node
-/*
- * Copyright 2023 Adobe. All rights reserved.
- * This file is licensed to you under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License. You may obtain a copy
- * of the License at http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software distributed under
- * the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR REPRESENTATIONS
- * OF ANY KIND, either express or implied. See the License for the specific language
- * governing permissions and limitations under the License.
- */
+import * as esbuild from 'esbuild';
+import { jsdomPatch } from './esbuild-plugins/jsdomPatch.cjs';
 
-import * as url from 'node:url';
-import * as path from 'node:path';
-import { fork } from 'node:child_process';
-import { existsSync } from 'node:fs';
-
-// read all directories, for those that have a webpack executable, run it with --mode production
-const webpackArgs = ['--mode', 'production'];
-const dirname = url.fileURLToPath(new URL('.', import.meta.url));
-
-const buildAction = async (dir) => {
-  const childDir = path.resolve(dirname, dir);
-  const webpackScript = path.resolve(childDir, 'node_modules/.bin/webpack');
-  if (existsSync(webpackScript)) {
-    return new Promise((resolve, reject) => {
-      // run the webpack script in the childDir
-      console.log(`Bundling ${dir} ... `);
-      const webpackProcess = fork(webpackScript, webpackArgs, {
-        cwd: childDir,
-        silent: false,
-      });
-      webpackProcess.on('error', (err) => reject(err));
-      webpackProcess.on('exit', (code) => {
-        if (code > 0) {
-          reject(code);
-        } else {
-          resolve();
-        }
-      });
-    }).catch((err) => {
-      console.log(`Failed bundling ${dir.name}.`);
-      throw err;
-    });
-  }
-  return Promise.resolve();
+const esbuildOptions = {
+  entryPoints: ['src/index.js'],
+  bundle: true,
+  outfile: 'dist/index.cjs',
+  platform: 'node',
+  target: 'node18',
+  external: ['canvas', 'bufferutil', 'utf-8-validate'],
+  plugins: [jsdomPatch],
+  logLevel: 'info',
 };
 
-// build curren
-buildAction('');
+export const buildAction = async () => esbuild.build(esbuildOptions);
+
+export const watchAction = async (onFirstBuild, onSubsequentBuilds) => {
+  const context = await esbuild.context({
+    ...esbuildOptions,
+    plugins: [
+      ...esbuildOptions.plugins,
+      {
+        // watch plugin that allows us to hook into the build process
+        name: 'watch',
+        setup(build) {
+          let count = 0;
+          build.onEnd(() => {
+            if (count === 0 && onFirstBuild) onFirstBuild();
+            else if (onSubsequentBuilds) onSubsequentBuilds();
+            count += 1;
+          });
+        },
+      },
+    ],
+  });
+  return context.watch();
+};
+
+// by default, build the actio.
+await buildAction();
