f930d0be492e8225771caf3c8b77213a9d3d63e6
gauthamksn <gsnair@adobe.com>
2023-10-12 15:06:54 +0530

    refactor

--- src/modules/blocks/create-code-block.js
+++ src/modules/blocks/create-code-block.js
@@ -14,55 +14,54 @@ function getLanguageClass(element) {
   return languageName || false;
 }
 
+function getClassArray(element) {
+  const blockOptions = [];
+
+  // Get hold of language information from <code> or <pre>
+  let language = getLanguageClass(element);
+  if (!language) {
+    language = getLanguageClass(element.parentNode);
+  }
+  // default case i.e if language is not specified then add language-none
+  if (!language) {
+    language = 'language-none';
+  }
+
+  blockOptions.push(language);
+
+  // if line number is available then get 'line-numbers'
+  if (element.parentNode.classList.contains('line-numbers'))
+    blockOptions.push('line-numbers');
+
+  // if start line number is available then get 'data-start'
+  if (element.parentNode.hasAttribute('data-start'))
+    blockOptions.push(
+      `data-start-${element.parentNode.getAttribute('data-start')}`,
+    );
+
+  // if data-line-offset is available then get 'data-line-offset'
+  if (element.parentNode.hasAttribute('data-line-offset'))
+    blockOptions.push(
+      `data-line-offset-${element.parentNode.getAttribute('data-line-offset')}`,
+    );
+
+  // if highlight line number is avialble then get 'data-line'
+  let highlightClass = '';
+  if (element.parentNode.hasAttribute('data-line')) {
+    blockOptions.highlight = element.parentNode.getAttribute('data-line');
+    blockOptions.highlight.split(',').forEach((h) => {
+      highlightClass = `h-${h.trim()} `;
+      blockOptions.push(highlightClass);
+    });
+  }
+
+  return blockOptions;
+}
+
 export default function createCodeBlock(document) {
   const codeElements = Array.from(document.querySelectorAll('pre > code'));
   codeElements.forEach((element) => {
-    const blockOptions = {};
-
-    // Get hold of language information from <code> or <pre>
-    let language = getLanguageClass(element);
-    if (!language) {
-      language = getLanguageClass(element.parentNode);
-    }
-
-    // default case i.e if language is not specified then add language-none
-    if (!language) {
-      language = 'language-none';
-    }
-
-    blockOptions.language = language;
-
-    // if line number is available then get 'line-numbers'
-    blockOptions.lineNumber = element.parentNode.classList.contains(
-      'line-numbers',
-    )
-      ? 'line-numbers'
-      : '';
-
-    // if start line number is available then get 'data-start'
-    blockOptions.startLine = element.parentNode.hasAttribute('data-start')
-      ? `data-start-${element.parentNode.getAttribute('data-start')}`
-      : '';
-
-    // if data-line-offset is available then get 'data-line-offset'
-    blockOptions.lineOffset = element.parentNode.hasAttribute(
-      'data-line-offset',
-    )
-      ? `data-line-offset-${element.parentNode.getAttribute(
-          'data-line-offset',
-        )}`
-      : '';
-
-    // if highlight line number is avialble then get 'data-line'
-    let highlightClass = '';
-    if (element.parentNode.hasAttribute('data-line')) {
-      blockOptions.highlight = element.parentNode.getAttribute('data-line');
-      blockOptions.highlight.split(',').forEach((h) => {
-        highlightClass += `h-${h.trim()} `;
-      });
-    } else {
-      highlightClass = '';
-    }
+    const blockOptions = getClassArray(element);
 
     const code = document.createElement('code');
     const pre = document.createElement('pre');
@@ -71,11 +70,7 @@ export default function createCodeBlock(document) {
 
     const cells = [[pre]];
 
-    const block = toBlock(
-      `Code (${blockOptions.language},${blockOptions.lineNumber}, ${blockOptions.startLine}, ${blockOptions.lineOffset}, ${highlightClass})`,
-      cells,
-      document,
-    );
+    const block = toBlock(`Code ${blockOptions.join(' ')}`, cells, document);
     element.parentNode.parentNode.replaceChild(block, element.parentNode);
   });
 }
