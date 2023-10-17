f39322ef63470285a3126c6f8dace96f1203bd58
mayukhs2904 <mayukhs@adobe.com>
2023-10-17 13:22:30 +0530

    video transcript added

---
+++ src/modules/blocks/create-video-transcript.js
@@ -0,0 +1,47 @@
+import { toBlock } from '../utils/dom-utils.js';
+
+export default function createVideoTranscript(document) {
+  const VideoTranscriptElements = Array.from(
+    document.getElementsByClassName('embed'),
+  );
+
+  VideoTranscriptElements.forEach((element) => {
+    const transcriptParagraph = element.nextElementSibling;
+    const cells = [];
+
+    if (
+      transcriptParagraph &&
+      transcriptParagraph.tagName === 'P' &&
+      transcriptParagraph.textContent.trim() === '+++ Transcript'
+    ) {
+      const div1 = document.createElement('div');
+      const transcript = transcriptParagraph.textContent.replace(/\+\+\+/g, '');
+      div1.textContent = transcript;
+      cells.push([div1]);
+
+      let currentParagraph = transcriptParagraph.nextElementSibling;
+      transcriptParagraph.remove();
+
+      while (currentParagraph) {
+        if (
+          currentParagraph.tagName === 'P' &&
+          currentParagraph.textContent.trim() === '+++'
+        ) {
+          currentParagraph.remove();
+          break;
+        }
+
+        const div = document.createElement('div');
+        div.textContent = currentParagraph.textContent;
+
+        cells.push([div]);
+        const nextParagraph = currentParagraph.nextElementSibling;
+        currentParagraph.remove();
+        currentParagraph = nextParagraph;
+      }
+    }
+
+    const block = toBlock(`VideoTranscript`, cells, document);
+    element.parentNode.insertBefore(block, element.nextSibling);
+  });
+}
