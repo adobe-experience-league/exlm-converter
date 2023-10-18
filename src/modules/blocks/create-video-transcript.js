83d4569638ca30a2747417e61b7ca358cd2ad821
mayukhs2904 <mayukhs@adobe.com>
2023-10-18 13:14:17 +0530

    condition added for length

--- src/modules/blocks/create-video-transcript.js
+++ src/modules/blocks/create-video-transcript.js
@@ -1,47 +1,52 @@
 import { toBlock } from '../utils/dom-utils.js';
 
 export default function createVideoTranscript(document) {
-  const VideoTranscriptElements = Array.from(
+  const videoTranscriptElements = Array.from(
     document.getElementsByClassName('embed'),
   );
 
-  VideoTranscriptElements.forEach((element) => {
-    const transcriptParagraph = element.nextElementSibling;
-    const cells = [];
+  if (videoTranscriptElements.length) {
+    videoTranscriptElements.forEach((element) => {
+      const transcriptParagraph = element.nextElementSibling;
+      const cells = [];
 
-    if (
-      transcriptParagraph &&
-      transcriptParagraph.tagName === 'P' &&
-      transcriptParagraph.textContent.trim() === '+++ Transcript'
-    ) {
-      const div1 = document.createElement('div');
-      const transcript = transcriptParagraph.textContent.replace(/\+\+\+/g, '');
-      div1.textContent = transcript;
-      cells.push([div1]);
+      if (
+        transcriptParagraph &&
+        transcriptParagraph.tagName === 'P' &&
+        transcriptParagraph.textContent.trim() === '+++ Transcript'
+      ) {
+        const div1 = document.createElement('div');
+        const transcript = transcriptParagraph.textContent.replace(
+          /\+\+\+/g,
+          '',
+        );
+        div1.textContent = transcript;
+        cells.push([div1]);
 
-      let currentParagraph = transcriptParagraph.nextElementSibling;
-      transcriptParagraph.remove();
+        let currentParagraph = transcriptParagraph.nextElementSibling;
+        transcriptParagraph.remove();
 
-      while (currentParagraph) {
-        if (
-          currentParagraph.tagName === 'P' &&
-          currentParagraph.textContent.trim() === '+++'
-        ) {
+        while (currentParagraph) {
+          if (
+            currentParagraph.tagName === 'P' &&
+            currentParagraph.textContent.trim() === '+++'
+          ) {
+            currentParagraph.remove();
+            break;
+          }
+
+          const div = document.createElement('div');
+          div.textContent = currentParagraph.textContent;
+
+          cells.push([div]);
+          const nextParagraph = currentParagraph.nextElementSibling;
           currentParagraph.remove();
-          break;
+          currentParagraph = nextParagraph;
         }
-
-        const div = document.createElement('div');
-        div.textContent = currentParagraph.textContent;
-
-        cells.push([div]);
-        const nextParagraph = currentParagraph.nextElementSibling;
-        currentParagraph.remove();
-        currentParagraph = nextParagraph;
       }
-    }
 
-    const block = toBlock(`Video Transcript`, cells, document);
-    element.parentNode.insertBefore(block, element.nextSibling);
-  });
+      const block = toBlock(`Video Transcript`, cells, document);
+      element.parentNode.insertBefore(block, element.nextSibling);
+    });
+  }
 }
