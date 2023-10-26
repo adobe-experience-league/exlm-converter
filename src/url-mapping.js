0da4639bdb4cf94c65fa9fead6a3c1ab3d9bdc5a
Ahmed <musallam@adobe.com>
2023-10-26 12:27:27 -0500

    Use URL article API and use mapping for internal URLs only

--- src/url-mapping.js
+++ src/url-mapping.js
@@ -1,33 +1,5 @@
 export default [
   {
-    path: '/docs/workfront/using/adobe-workfront-api/api-general-information/rich-text-field-api',
-    id: 'rechyRjUfs7xI4oZT',
-  },
-  {
-    path: '/docs/experience-platform/destinations/destination-sdk/functionality/destination-configuration/customer-authentication',
-    id: 'recZmWz9iUegew5Pn',
-  },
-  {
-    path: '/docs/journey-optimizer/using/ajo-home',
-    id: 'rec3NyHcJR7wJO8do',
-  },
-  {
-    path: '/docs/commerce-operations/configuration-guide/setup/initialization',
-    id: 'recslOZIKI2EKwjJl',
-  },
-  {
-    path: '/docs/data-workbench/using/hidden-test-page',
-    id: 'rec3qevEC7zbIeJco',
-  },
-  {
-    path: '/docs/integrations-learn/experience-cloud/solution-categories/content-management',
-    id: 'recrWO51UB125Pd29',
-  },
-  {
-    path: '/docs/analytics-learn/tutorials/analysis-workspace/navigating-workspace-projects/annotations-in-analysis-workspace',
-    id: 'recAjPiXNFnQj3jG3',
-  },
-  {
     path: '/docs/authoring-guide-exl/using/markdown/cheatsheet',
     id: 'recXZZxBo4pkOnx9k',
   },
@@ -35,64 +7,37 @@ export default [
     path: '/docs/authoring-guide-exl/using/markdown/syntax-style-guide',
     id: 'rec9WkO4eYWupFTrM',
   },
-  {
-    path: '/docs/experience-platform/landing/platform-apis/api-authentication',
-    id: 'reckcjRiLD5XYLFri',
-  },
-  {
-    path: '/docs/experience-platform/sources/ui-tutorials/create/cloud-storage/google-pubsub',
-    id: 'recSsOSKBiv9XioJA',
-  },
-  {
-    path: '/docs/journey-optimizer-learn/tutorials/introduction-to-journey-optimizer/mobile-capabilities-for-developers',
-    id: 'recGseE0j7HEteLlx',
-  },
-  {
-    path: '/docs/target-learn/tutorials/activities/create-ab-tests',
-    id: 'recCgLaXS1llwMbvb',
-  },
+
   {
     path: '/docs/authoring-guide-exl/using/authoring/tables',
     id: 'recSamvuJkILYVR1q  ',
   },
   {
-    path: '/docs/experience-manager-cloud-service/content/migration-journey/getting-started-partners',
-    id: 'rec9rN1MgLgR562Vh',
+    path: '/docs/authoring-guide-exl/using/authoring/linking',
+    id: 'recDHkKH0MjzPsrwq',
   },
   {
-    path: '/docs/creative-cloud-enterprise-learn/cce-learning-hub/cceoverview/overview-cce',
-    id: 'recUYHeFeLdTLak8z',
+    path: '/docs/authoring-guide-exl/using/authoring/using-metadata',
+    id: 'reczCFfhqIQlpEejb',
   },
   {
-    path: '/docs/creative-cloud-enterprise-learn/cce-learning-hub/cceoverview/ccerefguides/overview-ref',
-    id: 'rec7ajLazYrkI06nk',
+    path: '/docs/authoring-guide-exl/using/authoring/restructure-new',
+    id: 'recmQ0mtsvAYkwo4W',
   },
   {
-    path: '/docs/creative-cloud-enterprise-learn/cce-learning-hub/videooverview/overview-dva',
-    id: 'recLXJKP3A0JPghmz',
+    path: '/docs/authoring-guide-exl/using/authoring/includes',
+    id: 'rec3GuzJeOONUCEzJ',
   },
   {
-    path: '/docs/integrations-learn/experience-cloud/solution-categories/content-management',
-    id: 'recrWO51UB125Pd29',
+    path: '/docs/authoring-guide-exl/using/authoring/microsites',
+    id: 'recfe17eUvR7lnLCm',
   },
   {
-    path: '/docs/marketo/using/release-notes/previous-releases/2023/release-notes-sep-23',
-    id: 'rec1MHITzwBDe6dsf',
+    path: '/docs/authoring-guide-exl/using/authoring/contextual-help-popovers',
+    id: 'recwo9P3E9EjlToZo',
   },
   {
-    path: '/docs/marketo/using/product-docs/demand-generation/dynamic-chat/automated-chat/stream-designer',
-    id: 'recUQH1McIQ3uqRil',
-  },
-  {
-    path: '/docs/analytics/components/segmentation/seg-containers',
-    id: 'recsIiOJagHPjA6o5',
-  },
-  {
-    path: '/docs/experience-manager-guides-learn/tutorials/user-guide/manage-content/authoring-file-management',
-    id: 'recfxewptHHfJR6xn',
-  },
-  {
-    path: '/docs/blueprints-learn/architecture/customer-journeys/journey-optimizer/journey-optimizer',
-    id: 'recFhzRw6szJBc7Cq',
+    path: '/docs/authoring-guide-exl/using/authoring/courses',
+    id: 'rec6E43FbKY1GDdeb',
   },
 ];
