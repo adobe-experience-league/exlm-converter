1cf358831ed1eaa407968fb8f6d3b226210c0202
gauthamksn <gsnair@adobe.com>
2023-11-02 16:19:36 +0530

    refactor code

--- src/modules/blocks/create-tables.js
+++ src/modules/blocks/create-tables.js
@@ -4,6 +4,23 @@ import {
   groupWithParagraphs,
 } from '../utils/dom-utils.js';
 
+function rgbaToHex(rgba) {
+  let hex = '';
+  const match = rgba.match(
+    /^rgba?\((\d+),\s*(\d+),\s*(\d+),?\s*(\d*\.\d+|\d+)\)$/,
+  );
+  if (!match) {
+    return null;
+  }
+  for (let i = 1; i <= 3; i += 1) {
+    const component = parseInt(match[i], 10).toString(16);
+    hex += component.length === 1 ? `0${component}` : component;
+  }
+  const alpha = Math.round(parseFloat(match[4]) * 255).toString(16);
+  hex += alpha.length === 1 ? `0${alpha}` : alpha;
+  return hex;
+}
+
 /**
  * @param {Document} document
  */
@@ -14,6 +31,52 @@ export default function createTables(document) {
   if (tables.length) {
     tables.forEach((table) => {
       const variations = [];
+
+      // Number of cells in a row.
+      let cells = table.querySelectorAll('tr');
+      cells.forEach((cell, i) => {
+        variations.push(`${i}-row-${cell.children.length}`);
+      });
+
+      cells = table.querySelectorAll('th, td');
+      cells.forEach((cell, i) => {
+        // colspan variation
+        if (cell.getAttribute('colspan')) {
+          variations.push(`${i}-colspan-${cell.getAttribute('colspan')}`);
+        }
+
+        // rowspan variation
+        if (cell.getAttribute('rowspan')) {
+          variations.push(`${i}-rowspan-${cell.getAttribute('rowspan')}`);
+        }
+
+        // Cell text-align variation
+        if (cell.style.textAlign) {
+          variations.push(`${i}-align-${cell.style.textAlign}`);
+        }
+
+        // border variation
+        if (cell.style.border) {
+          variations.push(`${i}-border-${cell.style.border}`);
+        }
+
+        // background colour variation
+        if (cell.style.backgroundColor) {
+          variations.push(
+            `${i}-bgcolor-${rgbaToHex(cell.style.backgroundColor)}`,
+          );
+        }
+
+        // width variation
+        if (cell.getAttribute('width')) {
+          variations.push(`${i}-width-${cell.getAttribute('width')}`);
+        }
+
+        // height variation
+        if (cell.getAttribute('height')) {
+          variations.push(`${i}-height-${cell.getAttribute('height')}`);
+        }
+      });
 
       // Auto or Fixed variation
       if (table.style.tableLayout)
