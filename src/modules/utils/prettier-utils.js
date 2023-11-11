a047117d4360d2beee8b3f1e7a7b3008b1d24c2a
Ahmed <musallam@adobe.com>
2023-11-11 13:39:05 -0600

    better pretttier format options

--- src/modules/utils/prettier-utils.js
+++ src/modules/utils/prettier-utils.js
@@ -1,9 +1,11 @@
 import * as prettier from 'prettier/standalone';
-import prettierPluginHtml from 'prettier/plugins/html';
+import htmlPlugin from 'prettier/plugins/html';
 
 export async function formatHtml(html) {
   return prettier.format(html, {
     parser: 'html',
-    plugins: [prettierPluginHtml],
+    bracketSameLine: true,
+    printWidth: 150,
+    plugins: [htmlPlugin],
   });
 }
