eff6670e44e7dbd0b68bfececc6a6eba1e2578cb
Ruhi Singh <ruhis@adobe.com>
2024-01-19 20:14:41 +0530

    Fallback TOC logic for the article pages not having toc ID

--- src/modules/blocks/create-toc.js
+++ src/modules/blocks/create-toc.js
@@ -8,11 +8,12 @@ export default function createTOC(document, data) {
   // assume that it's the second div in the main section, always.
   const tocSection = document.querySelector('main > div:nth-child(2)');
   const fragment = document.createElement('a');
-
+  fragment.innerHTML = 'toc';
   if (data.toc) {
     const id = data.toc;
     fragment.href = `/toc/en/toc/${id}`;
-    fragment.innerHTML = 'toc';
+  }else{
+    fragment.href = '/fragments/en/toc/toc.html';
   }
   const cells = [[[fragment]]];
   const block = toBlock(`toc`, cells, document);
