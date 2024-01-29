d4748ae3d6f69f63c83c3616aa02f0e03a9ce3f6
Joseph Ross <joross@adobe.com>
2024-01-29 12:05:30 -0500

    Resolving conflict with createBreadcrumbs

--- src/modules/blocks/create-breadcrumbs.js
+++ src/modules/blocks/create-breadcrumbs.js
@@ -2,12 +2,12 @@ import yaml from 'js-yaml';
 import { toBlock } from '../utils/dom-utils.js';
 import { rewriteDocsPath } from '../utils/link-utils.js';
 
-export default function createBreadcrumbs(document, meta) {
+export default function createBreadcrumbs(document, meta, pageType, reqLang) {
   const headerElement = document.querySelector('h1');
   const metaDivTag = document.createElement('div');
   const fullMetadata = yaml.load(meta);
 
-  if (fullMetadata.breadcrumbs) {
+  if (pageType === DOCPAGETYPE.DOC_ARTICLE && fullMetadata.breadcrumbs) {
     const breadcrumbs = JSON.parse(fullMetadata.breadcrumbs) || [];
     
     breadcrumbs.forEach((breadcrumb, index, array) => {
@@ -26,8 +26,26 @@ export default function createBreadcrumbs(document, meta) {
     });
   }
 
+  if (pageType === DOCPAGETYPE.SOLUTION_LANDING) {
+    const docsTitle = fullMetadata.type ? fullMetadata.type : 'Documentation';
+    const productTitle = fullMetadata['breadcrumb-name']
+      ? fullMetadata['breadcrumb-name']
+      : fullMetadata.solution;
+
+    if (docsTitle) {
+      const docAnchorTag = document.createElement('a');
+      docAnchorTag.setAttribute('href', `/${reqLang}/docs`);
+      docAnchorTag.textContent = docsTitle;
+      metaDivTag.append(docAnchorTag);
+    }
+
+    if (productTitle) {
+      const productAnchorTag = document.createElement('a');
+      productAnchorTag.textContent = productTitle;
+      metaDivTag.append(productAnchorTag);
+    }
+  }
   const cells = [[metaDivTag]];
   const block = toBlock('breadcrumbs', cells, document);
-  
   headerElement.parentNode.insertBefore(block, headerElement);
 }
