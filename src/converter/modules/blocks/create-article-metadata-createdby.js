06bbee2946a96d8ada37306badc56a26fc28d262
Ruhi Singh <ruhis@adobe.com>
2024-02-08 16:33:01 +0530

    EXLM-1092 UI Fixes

--- src/converter/modules/blocks/create-article-metadata-createdby.js
+++ src/converter/modules/blocks/create-article-metadata-createdby.js
@@ -8,40 +8,45 @@ export default function createArticleMetaDataCreatedBy(
 ) {
   // Article Metadata Created For
   if (data.Level || data.Role) {
-    const metaElement = document.querySelector('.article-metadata');
-    const levelDivTag = document.createElement('div');
-    const createdForDiv = document.createElement('div');
-    const paragraph = document.createElement('p');
-    const parentDiv = document.createElement('div');
-    paragraph.textContent = CREATED_FOR[`${reqLang}`];
-    createdForDiv.append(paragraph);
-    parentDiv.append(createdForDiv);
-    const items = [];
-    if (data.Level && data.Level !== '') {
-      const levels = data.Level;
-      levels.forEach((tags) => {
-        if (tags.trim() !== '') {
-          items.push(tags.trim());
-        }
-      });
+    if (Array.isArray(data.Level) && Array.isArray(data.Role)) {
+      const levelsArray = data.Level;
+      const rolesArray = data.Role;
+      if (
+        !(levelsArray.length === 1 && levelsArray[0] === '') ||
+        !(rolesArray.length === 1 && rolesArray[0] === '')
+      ) {
+        const metaElement = document.querySelector('.article-metadata');
+        const levelDivTag = document.createElement('div');
+        const parentDiv = document.createElement('div');
+
+        const createdForDiv = document.createElement('div');
+        const paragraph = document.createElement('p');
+        paragraph.textContent = CREATED_FOR[`${reqLang}`];
+        createdForDiv.append(paragraph);
+        parentDiv.append(createdForDiv);
+        const items = [];
+
+        levelsArray.forEach((tags) => {
+          if (tags.trim() !== '') {
+            items.push(tags.trim());
+          }
+        });
+        rolesArray.forEach((role) => {
+          if (role.trim() !== '') {
+            items.push(role.trim());
+          }
+        });
+        levelDivTag.append(
+          newHtmlList(document, {
+            tag: 'ul',
+            items,
+          }),
+        );
+        parentDiv.append(levelDivTag);
+        const cells = [[parentDiv]];
+        const block = toBlock('article-metadata-createdby', cells, document);
+        metaElement.insertAdjacentElement('afterend', block);
+      }
     }
-    if (data.Role && data.Role !== '') {
-      const roles = data.Role;
-      roles.forEach((role) => {
-        if (role.trim() !== '') {
-          items.push(role.trim());
-        }
-      });
-    }
-    levelDivTag.append(
-      newHtmlList(document, {
-        tag: 'ul',
-        items,
-      }),
-    );
-    parentDiv.append(levelDivTag);
-    const cells = [[parentDiv]];
-    const block = toBlock('article-metadata-createdby', cells, document);
-    metaElement.insertAdjacentElement('afterend', block);
   }
 }
