5bc3b90253ffdb713f1ec9caf1a90dc8ffb60430
Ahmed <musallam@adobe.com>
2024-02-23 13:26:19 -0600

    replace text nodes

--- src/converter/modules/blocks/create-highlight.js
+++ src/converter/modules/blocks/create-highlight.js
@@ -1,11 +1,26 @@
-import { replaceElement } from '../utils/dom-utils.js';
+import {
+  getAllDecendantTextNodes,
+  replaceElement,
+} from '../utils/dom-utils.js';
+
+/**
+ *
+ * @param {Document} document
+ * @param {Node} node
+ */
+const toHighlightedEl = (document, textNode) => {
+  const em = document.createElement('em');
+  const u = document.createElement('u');
+  em.appendChild(u);
+  u.innerHTML = textNode.textContent;
+  return em;
+};
 
 export default function createHighlight(document) {
-  [...document.querySelectorAll('.preview')].forEach((spanEl) => {
-    const em = document.createElement('em');
-    const u = document.createElement('u');
-    em.appendChild(u);
-    u.innerHTML = spanEl.innerHTML;
-    replaceElement(spanEl, em);
+  [...document.querySelectorAll('.preview')].forEach((previewEl) => {
+    getAllDecendantTextNodes(document, previewEl).forEach((textNode) => {
+      const highlightEl = toHighlightedEl(document, textNode);
+      replaceElement(textNode, toHighlightedEl(document, highlightEl));
+    });
   });
 }
