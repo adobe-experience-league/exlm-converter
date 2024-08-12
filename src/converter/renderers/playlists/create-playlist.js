e38d4189d30f98d4836990296568699b64013fce
Ahmed <musallam@adobe.com>
2024-08-12 15:24:05 -0500

    pick thumbnail image that is at-least 900px

--- src/converter/renderers/playlists/create-playlist.js
+++ src/converter/renderers/playlists/create-playlist.js
@@ -1,6 +1,31 @@
 import { htmlToElement, toBlock } from '../../modules/utils/dom-utils.js';
 
 /**
+ * Thumbnail urls are expecte to have -<number>x<number>
+ * Example: https://images-tv.adobe.com/<long hash>-960x491.jpg
+ * which means the image is 960px width and 491px height
+ * This function will get the thumbnail url that is just above 900px width
+ * if there is no thumbnail above 900px width, it will get the largest thumbnail
+ * @param {string[]} thumbnailUrls
+ */
+function getThumbnail(thumbnailUrls, minWidth = 900) {
+  const sizes = thumbnailUrls
+    .map((url) => url.match(/-\d+x\d+/g))
+    .map((url) => (url[0] ? url[0].replace('-', '') : ''))
+    .filter((size) => size)
+    // parse sizes to width and height
+    .map((size) => size.split('x').map(Number))
+    // sort by width
+    .sort((a, b) => a[0] - b[0]);
+
+  // get the value just above 900 width
+  const [width, height] =
+    sizes.find((size) => size[0] > minWidth) || sizes.pop();
+
+  return thumbnailUrls.find((url) => url.includes(`${width}x${height}`)) || '';
+}
+
+/**
  * Creates a video row for playlist block
  * @param {Document} document
  * @param {Video} Video
@@ -23,8 +48,9 @@ function videoRow(document, Video) {
 
   const videoP = newPLink(URL);
   const videoTranscriptP = newPLink(TranscriptURL);
-  const thumbnailUrl =
-    thumbnailUrls.find((url) => url.includes('960x540')) || '';
+
+  const thumbnailUrl = getThumbnail(thumbnailUrls);
+
   const thumbnail = newEl(`<img src="${thumbnailUrl}" alt="${Title}">`);
   const title = newEl(`<h3>${Title}</h3>`);
   const desc = newEl(`<p>${Description}</p>`);
