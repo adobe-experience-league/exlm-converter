72d14e093a0300793dedecb03610584ee42c4f05
gauthamksn <gsnair@adobe.com>
2025-01-10 15:32:17 +0530

    refactor

--- src/converter/renderers/utils/tag-translation-utils.js
+++ src/converter/renderers/utils/tag-translation-utils.js
@@ -8,14 +8,16 @@ const MAPPING_TO_TRANSLATE = [
   },
 ];
 
-// TODO: List of tags key mapping
-// const TAGS_LABEL_MAPPING = {
-//     "roles": "role",
-//     "levels": "level",
-//     "features": "feature",
-//     "topics": "topic",
-//     "solution": "solution",
-// }
+// List of tags key mapping
+const TAGS_LABEL_MAPPING = {
+  'content-type': 'types',
+  'experience-level': 'levels',
+  feature: 'features',
+  industry: 'industries',
+  role: 'roles',
+  solution: null,
+  topic: 'topics',
+};
 
 /**
  * formattedTags returns the array of base64 encoded tags after extracting from the tags selected in dialog
@@ -50,6 +52,14 @@ function formattedTags(inputString) {
   return resultArray;
 }
 
+/**
+ * Translate the required tags in a block
+ * (Update MAPPING_TO_TRANSLATE in tag-translation-utils.js
+ * to add new tags for translation).
+ * @param {HTMLAllCollection} document - The document object.
+ * @param {string} lang - The language to translate to.
+ * @summary This function adds a new div with the translated tags to the block.
+ */
 export const translateBlockTags = async (document, lang) => {
   await Promise.all(
     MAPPING_TO_TRANSLATE.map(async (blockDetails) => {
@@ -60,18 +70,33 @@ export const translateBlockTags = async (document, lang) => {
       const rawTags =
         block.children[blockDetails.tagsPosition].firstElementChild.textContent;
 
-      const translatedTagsPromises = formattedTags(rawTags).map(
-        async (rawTag) => {
+      // Wait for all tag translations to complete
+      const translatedTags = await Promise.all(
+        formattedTags(rawTags).map(async (rawTag) => {
           const values = rawTag.split('/').reverse();
-          const tag = values[0].trim();
-          const key = `${values.pop().replace('exl:', '').trim()}s`; // Create a mapping for the key
-          return defaultExlClient.getLabelFromEndpoint(key, tag, lang);
-        },
+          const tag = values.shift().trim();
+          const key = `${values.pop().replace('exl:', '').trim()}`;
+          const keyMapping = TAGS_LABEL_MAPPING[key];
+          const solution = values.length ? values[0] : '';
+          if (!keyMapping) return { solution, key, tag, result: tag };
+          const result = await defaultExlClient.getLabelFromEndpoint(
+            keyMapping,
+            tag,
+            lang,
+          );
+          return { solution, key, tag, result };
+        }),
       );
 
-      // Wait for all tag translations to complete
-      const translatedTags = await Promise.all(translatedTagsPromises);
-      const tags = translatedTags.join(',');
+      // Create the tags string in format : [ feature : tag_en : tag_translated : solution if present ]
+      const tags = translatedTags
+        .map((translatedTag) => {
+          const { solution, key, tag, result } = translatedTag;
+          return solution
+            ? `${key}/${solution}/${tag}:${result}:`
+            : `${key}/${tag}:${result}:`;
+        })
+        .join(',');
 
       // Create and append the translated tags container
       const tagsContainer = document.createElement('div');
