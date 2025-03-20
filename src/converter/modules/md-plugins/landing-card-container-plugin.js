91afd7c6eef4f4d4f639adf2f40309b7d0618608
Lakshay Sharma <lakshays@adobe.com>
2025-03-20 16:20:38 -0500

    refactor: custom block changes

--- src/converter/modules/md-plugins/landing-card-container-plugin.js
+++ src/converter/modules/md-plugins/landing-card-container-plugin.js
@@ -6,17 +6,9 @@ export default function landingCardContainerPlugin(md) {
       return params.trim().match(/^landing-card-container\s*(.*)$/);
     },
     render(tokens, idx) {
-      const match = tokens[idx].info
-        .trim()
-        .match(/^landing-card-container\s*(.*)$/);
       if (tokens[idx].nesting === 1) {
         let html = '<div class="landing-card-container">\n';
-        html += '<div class="landing-card-content">\n';
-        if (match && match[1]) {
-          html += `<div class="landing-card-title">${md.utils.escapeHtml(
-            match[1],
-          )}</div>\n`;
-        }
+        html += '<div>\n';
         return html;
       }
       return '</div>\n</div>\n';
