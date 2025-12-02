41887b93244d0de78edd3de28cb23c64f081d34b
Sivaram Vargheese Thangam <sivaramvt@gmail.com>
2025-12-02 21:48:30 +0530

    Stage --> Prod (#655)
    * feat: add Coveo token service with Vault integration
    
    * Satisfying prettier
    
    * localhost uri /coveo-token
    
    * added .aem.page & .aem.live in allowed patterns
    
    * added .hlx.page & .hlx.live in allowed pattens
    
    * allowed patterns update
    
    * CORS issue solution
    
    * CORS issue solution
    
    * allowed patterns update
    
    * allow local host
    
    * error message change
    
    * update error through sendError
    
    * 403 error message handeling
    
    * 403 error message handeling
    
    * Dev -> Stage (#648)
    
    * Setting up cache header for HTTP response
    
    Enable cache header "Cache-Control": "max-age=3600"
    
    * Prettier change
    
    * allow adobe io (#649)
    
    * Exlm 4396 (#650)
    
    * allow adobe io
    
    * Update index.js
    
    Cache header update for coveo token action - Status 200
    
    ---------
    
    Co-authored-by: priyanshij2 <priyanshij@adobe.com>
    
    ---------
    
    Co-authored-by: Sivaram Vargheese Thangam <sivaramvt@gmail.com>
    Co-authored-by: priyanshij2 <priyanshij@adobe.com>
    
    * Dev --> Stage (#654)
    
    * Setting up cache header for HTTP response
    
    Enable cache header "Cache-Control": "max-age=3600"
    
    * Prettier change
    
    * allow adobe io (#649)
    
    * Exlm 4396 (#650)
    
    * allow adobe io
    
    * Update index.js
    
    Cache header update for coveo token action - Status 200
    
    ---------
    
    Co-authored-by: priyanshij2 <priyanshij@adobe.com>
    
    * Coveo token - additional header for helix (#653)
    
    ---------
    
    Co-authored-by: ruhisingh1 <145125444+ruhisingh1@users.noreply.github.com>
    Co-authored-by: priyanshij2 <priyanshij@adobe.com>
    
    ---------
    
    Co-authored-by: Sivaram <sivt@adobe.com>
    Co-authored-by: priyanshij2 <priyanshij@adobe.com>
    Co-authored-by: ruhisingh1 <145125444+ruhisingh1@users.noreply.github.com>
    Co-authored-by: noormd08 <140145460+noormd08@users.noreply.github.com>

--- src/coveo/index.js
+++ src/coveo/index.js
@@ -243,6 +243,7 @@ export const main = async function main(params) {
           'Access-Control-Allow-Methods': 'GET, OPTIONS',
           'Access-Control-Allow-Headers': 'Content-Type',
           'Cache-Control': 'max-age=3600',
+          'Edge-Control': 'max-age=3600',
         },
         body: {
           token,
@@ -268,6 +269,7 @@ export const main = async function main(params) {
           'Access-Control-Allow-Methods': 'GET, OPTIONS',
           'Access-Control-Allow-Headers': 'Content-Type',
           'Cache-Control': 'max-age=3600',
+          'Edge-Control': 'max-age=3600',
         },
         body: {
           token: localToken,
